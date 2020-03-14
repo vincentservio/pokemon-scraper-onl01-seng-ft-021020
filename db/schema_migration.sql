8709ade752e521568c6ba8c11e074d0d6e2999de
Vincent Servio <vincentservio@gmail.com>
2020-03-14 17:11:28 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -3,5 +3,3 @@ ALTER TABLE pokemon ADD hp INTEGER DEFAULT 60;
 -- It should create a table in a new table in the db/ directory.
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
 
-
-https://learn.co/tracks/online-software-engineering-structured/orms-and-activerecord/orms/pokemon-scraper-lab#
